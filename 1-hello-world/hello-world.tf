132110a98ad47b716a498237b67175b629974b68
CloudGee <57861564+CloudGee@users.noreply.github.com>
2025-06-14 07:57:11 +0000

    hello world

---
+++ 1-hello-world/hello-world.tf
@@ -0,0 +1,24 @@
+# hello-world.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-west-2"
+  # aws modle will use the default credentials from the environment or AWS CLI config, env name:
+  # AWS_PROFILE
+}
+
+resource "aws_vpc" "aws" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true 
+  tags = {
+    Name = "hello-world-vpc-1"
+  }
+}
--- set-env.sh
+++ set-env.sh
@@ -1,12 +1,35 @@
-// a script to read input from command line and set aws ak/sk to environment variables
+# set-env.sh
+# a script to read input from command line and set aws ak/sk/role to a profile, then use that profile
+
 #!/bin/bash
+
+# Unset all AWS_* environment variables
+for var in $(env | grep '^AWS_' | awk -F= '{print $1}'); do
+    unset $var
+done
+
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <AWS_ACCESS_KEY_ID> <AWS_SECRET_ACCESS_KEY>"
-    exit 1
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <AWS_ACCESS_KEY_ID> <AWS_SECRET_ACCESS_KEY> <AWS_ROLE_ARN>"
+    exit 0
 fi
-# Set the AWS access key and secret access key as environment variables
-export AWS_ACCESS_KEY_ID="$1"
-export AWS_SECRET_ACCESS_KEY="$2"
-# Print a message indicating that the environment variables have been set
-echo "AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY have been set."
+
+AWS_ACCESS_KEY_ID="$1"
+AWS_SECRET_ACCESS_KEY="$2"
+AWS_ROLE_ARN="$3"
+BASE_PROFILE="base_profile"
+ASSUME_PROFILE="assume_role_profile"
+
+# Configure the base profile using awscli
+aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID" --profile "$BASE_PROFILE"
+aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY" --profile "$BASE_PROFILE"
+
+# Write role_arn and source_profile to ~/.aws/config
+aws configure set region us-west-2 --profile "$ASSUME_PROFILE"
+aws configure set role_arn "$AWS_ROLE_ARN" --profile "$ASSUME_PROFILE"
+aws configure set source_profile "$BASE_PROFILE" --profile "$ASSUME_PROFILE"
+
+# Set AWS_PROFILE environment variable
+export AWS_PROFILE="$ASSUME_PROFILE"
+
+echo "AWS profile '$ASSUME_PROFILE' with role_arn has been configured and is now active."
